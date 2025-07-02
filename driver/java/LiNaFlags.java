c1ad88cdb56866535f2acdf6e9657e9c36dfa072
debugdoctor <aime_rick@163.com>
2025-07-02 11:12:14 +0800

    feat: add drivers prototype

---
+++ driver/java/LiNaFlags.java
@@ -0,0 +1,18 @@
+public enum LiNaFlags {
+    DELETE(0xC0),
+    WRITE(0x80),
+    READ(0x40),
+    COVER(0x02),
+    COMPRESS(0x01),
+    NONE(0x00);
+
+    private final int value;
+
+    LiNaFlags(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
